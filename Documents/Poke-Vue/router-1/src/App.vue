67e59378f21db3cc7edbda7b81a45ac6292f6a37
HOOCX <hansoliver23@gmail.com>
2024-05-20 23:15:31 -0400

    pinia ready

---
+++ Documents/Poke-Vue/router-1/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  
+  <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    
+    <router-link class="navbar-brand" to="/">
+      <img src="@/assets/logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      PokeAPI
+    </router-link>
+    <div>
+      <router-link class="btn btn-outline-primary me-2" to="/">Home</router-link>
+      <router-link to="/pokemons" class="btn btn-outline-primary me-2">Pokemons</router-link>
+      <router-link to="/favoritos" class="btn btn-outline-primary">Favoritos</router-link>
+    </div>
+  </div>
+</nav>
+  
+ <div class="container text-center">
+  <RouterView />
+</div>
+  
+</template>
+
