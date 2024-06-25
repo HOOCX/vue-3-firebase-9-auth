41a2bd18ee9f0ac69e441bafa154f9305eddbf3a
HOOCX <hansoliver23@gmail.com>
2024-06-25 19:58:33 -0400

    Auth so so

---
+++ Documents/vue-projects/firebase-app-1/src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBR3UYqU0gbjbMpFMX8cdChA6ijCEo89fM",
+  authDomain: "vue-3-2024-3f4c6.firebaseapp.com",
+  projectId: "vue-3-2024-3f4c6",
+  storageBucket: "vue-3-2024-3f4c6.appspot.com",
+  messagingSenderId: "969462138158",
+  appId: "1:969462138158:web:ab3f6a8fde74a9e259a6bf"
+};
+
+ initializeApp(firebaseConfig);
+ const auth = getAuth()
+
+ export default auth;
+
+
